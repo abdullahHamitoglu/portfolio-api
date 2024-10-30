2d9f17afb155e78208a30506ef69e2b02f1f7d1d
abdullah alhammad <abdullah.developer97@gmail.com>
2024-10-30 12:40:14 +0300

    {commit_message}

--- dist/database/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const mongoUri = process.env.MONGODB_URI;
-// Connect to MongoDB
-(0, mongoose_1.connect)(mongoUri)
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error(err));
-//# sourceMappingURL=index.js.map
